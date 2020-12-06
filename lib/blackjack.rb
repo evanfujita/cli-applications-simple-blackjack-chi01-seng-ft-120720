49c0205f5b2cb9106efa0640f8b5d88e51673888
Evan Fujita <evanfujita@gmail.com>
2020-12-06 21:22:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,9 +29,12 @@ def initial_round
   card_total
 end
 
-def hit?
+def hit?(card_total)
   prompt_user
-  get_user_input
+  input = get_user_input
+    if input == "h"
+      deal_card
+      input == "s"
 end
 
 def invalid_command
