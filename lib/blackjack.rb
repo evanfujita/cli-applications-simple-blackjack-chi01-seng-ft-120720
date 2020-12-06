fe94f495a5d6d69e9dfce61f5e58224f60bac59d
Evan Fujita <evanfujita@gmail.com>
2020-12-06 22:04:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,9 +54,9 @@ end
 def runner
   welcome 
   card_total = initial_round
-  if card_total < 21
+  while card_total < 21
       hit?(card_total)
   end
-      display_card_total(card_total)   
+  display_card_total(card_total)   
 end
     
