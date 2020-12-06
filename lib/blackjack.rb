145f929f1e6f27b96c7c7f1f758c6e3f48f0185d
Evan Fujita <evanfujita@gmail.com>
2020-12-06 21:15:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,9 @@ def end_game(card_total)
 end
 
 def initial_round
-  sum = deal_card
-  sum += deal_card
+  card_total = deal_card
+  card_total += deal_card
+  display_card_total(card_total)
 end
 
 def hit?
