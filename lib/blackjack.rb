940aa31da8c162072388d651653ccd2f7b3984c4
Evan Fujita <evanfujita@gmail.com>
2020-12-06 21:14:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,7 +19,7 @@ def get_user_input
 end
 
 def end_game
-  puts "I'm sorry, your card total is #{card_total}. Thank you for playing."
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
